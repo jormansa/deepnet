87f547fc4d46d5e1e3b31fa4621b9d07b86b7d17
Jordi <jodilocal@sift.(none)>
2014-06-17 15:41:56 +0200

    program to calculate num samples

---
+++ deepnet/num_samples.py
@@ -0,0 +1,16 @@
+import glob
+import numpy as np
+
+proto = sys.argv[1]
+filenames = []
+filenames.append(sorted(glob.glob(proto)))
+numfiles = len(filenames[0])
+assert numfiles > 0, 'num files = %d' % (numfiles)
+
+total_samples = 0
+for i in range(0, numfiles):
+	curr = np.load(filenames[0][i])
+	curr_size = curr.shape[0]
+	total_samples = total_samples + curr_size
+	print 'File %s with %d samples' % (filenames[0][i], curr_size)
+
