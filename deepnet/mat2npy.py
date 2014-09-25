873ea1bddd7477c48a2a4546e3e30c2bfd719511
jormansa <gasoljugon@hotmail.com>
2014-09-25 10:45:40 +0200

    convert several files all at once

--- deepnet/mat2npy.py
+++ deepnet/mat2npy.py
@@ -2,18 +2,24 @@ import numpy as np
 import scipy.io as sio
 import os
 import sys
+import glob
 
 # convert .npy to .mat, or viceversa
 # THE KEY MUST HAVE THE SAME NAME AS THE FILE
 
 to_convert = sys.argv[1]
-fileName, fileExtension = os.path.splitext(to_convert)
-if fileExtension == '.mat':
-	mat_contents = sio.loadmat(to_convert)
-	data = mat_contents[fileName]
-	np.save(fileName + '.npy', data)
-	print 'Converted %s to %s.npy' % (to_convert,fileName)
-else:
-	data = np.load(to_convert)
-	sio.savemat(fileName + '.mat',mdict={fileName: data},do_compression=True)
-	print 'Converted %s to %s.mat' % (to_convert,fileName)
+filenames = []
+filenames.append(sorted(glob.glob(to_convert)))
+numfiles = len(filenames[0])
+assert numfiles > 0, 'no files to convert'
+for i in range(0, numfiles):
+	fileName, fileExtension = os.path.splitext(filenames[0][i])
+	if fileExtension == '.mat':
+		mat_contents = sio.loadmat(filenames[0][i])
+		data = mat_contents[fileName]
+		np.save(fileName + '.npy', data)
+		print 'Converted %s to %s.npy' % (filenames[0][i],fileName)
+	else:
+		data = np.load(filenames[0][i])
+		sio.savemat(fileName + '.mat',mdict={fileName: data},do_compression=True)
+		print 'Converted %s to %s.mat' % (filenames[0][i],fileName)
